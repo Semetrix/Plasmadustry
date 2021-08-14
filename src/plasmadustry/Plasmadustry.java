ae9e4d3d6702738cd469f047bb5568f9f0a1d107
KayAyeAre <56635816+KayWasTaken@users.noreply.github.com>
2021-08-14 19:57:48 +0700

    Update Plasmadustry.java

--- src/plasmadustry/Plasmadustry.java
+++ src/plasmadustry/Plasmadustry.java
@@ -12,7 +12,7 @@ public class Plasmadustry extends Mod{
 
     public Plasmadustry(){
         Log.info("Loaded Plasmadustry constructor.");
-        public static final String shortName = "The state which nerds talk about in school"
+        public static final String shortName = "Its a bunch of gas, really."
     @Override
     public void loadContent(){
 		Log.info("Loading some example content.");
